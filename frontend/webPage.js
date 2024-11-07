61b920f1c09664de3a23fb9670e675e2fe49aab9
sunolr <srasapatirath1@csub.edu>
2024-11-07 14:37:19 -0800

    pushing

--- frontend/webPage.js
+++
@@ -1,9 +0,0 @@
-import { Provider } from "@/components/ui/provider"
-
-function App({ Component, pageProps }) {
-  return (
-    <Provider>
-      <Component {...pageProps} />
-    </Provider>
-  )
-}
