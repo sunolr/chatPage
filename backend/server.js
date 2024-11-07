17824b2de8cd24315195ac77fdeb8e0369b80e1b
sunolr <srasapatirath1@csub.edu>
2024-11-07 14:02:30 -0800

    push

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const path = require("path");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 // Serve static files from the 'chatPage' directory
 app.use(express.static(path.join(__dirname, '../')));  // Move one level up to access 'chatPage' directory
@@ -11,11 +12,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../webPage.html'));  // Serve webPage.html
 });
 
-app.get('/', (req, res) => {
-    res.send("Api is running");
-});
+
+const PORT = process.env.PORT || 5000
 
 // Start the server
-app.listen(443, () => {
-    console.log("Server started on port 443");
+app.listen(PORT, () => {
+    console.log(`Server started on PORT ${PORT}`);
 });
