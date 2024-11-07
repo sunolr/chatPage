d1b67e93c6c53f1679c37e306d9d7ed60458b7e2
sunolr <srasapatirath1@csub.edu>
2024-11-07 13:43:54 -0800

    pushing

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../webPage.html'));  // Serve webPage.html
 });
 
+app.get('/', (req, res) => {
+    res.send("Api is running");
+});
+
 // Start the server
 app.listen(443, () => {
     console.log("Server started on port 443");
